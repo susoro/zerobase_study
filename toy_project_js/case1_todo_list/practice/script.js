779d6a542e8cba2d2ddc856c61e24df5479d0aae
sugyeongsu <78544372+susoro@users.noreply.github.com>
2024-05-28 22:44:11 +0900

    데이터 가져오기 완료

--- toy_project_js/case1_todo_list/practice/script.js
+++ toy_project_js/case1_todo_list/practice/script.js
@@ -1,15 +1,18 @@
 ;(function () {
-  'use strict'
+  'use strict' // 엄격 모드 사용: 자바스크립트의 오류를 더 엄격하게 체크합니다.
 
+  // DOM 요소를 선택하는 헬퍼 함수
   const get = (target) => {
     return document.querySelector(target)
   }
+  const $todos = get('.todos') // .todos 클래스를 가진 요소를 선택
 
+  // 할 일(todo) 요소를 생성하는 함수
   const createTodoElement = (item) => {
-    const { id, content } = item
-    const $todoItem = document.createElement('div')
-    $todoItem.classList.add('item')
-    $todoItem.dataset.id = id
+    const { id, content } = item // item 객체에서 id와 content를 구조 분해 할당
+    const $todoItem = document.createElement('div') // 새로운 div 요소 생성
+    $todoItem.classList.add('item') // 'item' 클래스를 추가
+    $todoItem.dataset.id = id // 데이터 속성에 id를 설정
     $todoItem.innerHTML = `
             <div class="content">
               <input
@@ -36,9 +39,33 @@
               </button>
             </div>
       `
-    return $todoItem
+    return $todoItem // 생성된 할 일 요소를 반환
   }
 
+  // 모든 할 일을 렌더링하는 함수
+  const renderAllTodos = (todos) => {
+    $todos.innerHTML = '' // 기존 내용을 비웁니다.
+    todos.forEach((item) => {
+      const todoElement = createTodoElement(item) // 각 할 일 아이템에 대해 요소를 생성
+      $todos.appendChild(todoElement) // 생성된 요소를 .todos 요소에 추가
+    })
+  }
+
+  // 할 일 목록을 서버에서 가져오는 함수
+  const getTodos = () => {
+    fetch('http://localhost:3000/todos') // 서버에 GET 요청을 보냅니다.
+    .then((response) => response.json()) // 응답을 JSON 형태로 변환
+    .then((todos) => {renderAllTodos(todos)}) // 변환된 데이터를 이용해 모든 할 일을 렌더링
+    .catch(error => console.error(error)) // 오류가 발생하면 콘솔에 출력   
+  }
+
+  // 초기화 함수 (현재는 아무 작업도 하지 않음)
   const init = () => {}
-  init()
+  
+  // DOMContentLoaded 이벤트가 발생하면 getTodos 함수를 호출
+  window.addEventListener('DOMContentLoaded',() =>{
+    getTodos()
+  })
+  
+  init() // 초기화 함수 호출 (현재는 아무 작업도 하지 않음)
 })()
